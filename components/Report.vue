8aae129a94a0d19a5f0e36cd0076e36986e852be
Bruce Crevensten <bruce.crevensten@gmail.com>
2021-10-26 16:02:11 -0700

    Adds deltas to charts, switches default to imperial units, closes #26, #27, #20, #18.

--- components/Report.vue
+++ components/Report.vue
@@ -14,9 +14,9 @@
 			</div>
 			<hr />
 			<h3 class="title is-4 place">
-				Projected Future Conditions for <span v-html="place"></span>
+				Projected Conditions for <span v-html="place"></span>
 			</h3>
-			
+
 			<div v-if="$fetchState.pending">
 				<!-- Drama dots -->
 				<h4 class="title is-5">Loading data&hellip;</h4>
@@ -40,12 +40,25 @@
 			<div v-else>
 				<!-- The report! -->
 				<div class="report--minimap--wrapper">
-					<MiniMap/>
+					<MiniMap />
 				</div>
 				<div class="report-controls">
 					<div class="content is-size-5">
-						<p>The tables and charts below are specific to the selected point, or have been averaged across the selected HUC or ecoregion.  Note that the precision of these results depends on the grid resolution (pixel size) of the underlying dataset.  Also note that some data layers are not available as point data or at the HUC or ecoregion level.</p>
-						<p>These data have been averaged by season. <strong>Winter</strong> is December, January and February. <strong>Spring</strong> is March, April and May. <strong>Summer</strong> is June, July and August, and <strong>fall</strong> is September, October and November.</p>
+						<p>
+							The tables and charts below are specific to the selected point, or
+							have been averaged across the selected HUC or ecoregion. Note that
+							the precision of these results depends on the grid resolution
+							(pixel size) of the underlying dataset. Also note that some data
+							layers are not available as point data or at the HUC or ecoregion
+							level.
+						</p>
+						<p>
+							These data have been averaged by season.
+							<strong>Winter</strong> is December, January and February.
+							<strong>Spring</strong> is March, April and May.
+							<strong>Summer</strong> is June, July and August, and
+							<strong>fall</strong> is September, October and November.
+						</p>
 					</div>
 					<b-field label="Units">
 						<b-radio v-model="units" name="units" native-value="imperial">
@@ -64,12 +77,41 @@
 				</div>
 				<div class="content is-size-5">
 					<p>
-						Comparing projections with historical data in the first column offers broad insights into future trends.  Comparisons between models and RCPs offers a range of uncertainty with regard to these trends.  It is important to note that even small changes in precipitation and temperature may result in profound landscape effects, particularly if threshold values are crossed.  For example, a shift from below-freezing to above-freezing temperature can alter permafrost and active layer, while hotter sping and summer temperatures can increase fire frequency.  Temperature and precipitation data are used as inputs in many other models.
+						Comparing projections with historical data in the first column
+						offers broad insights into future trends. Comparisons between models
+						and RCPs offers a range of uncertainty with regard to these trends.
+						It is important to note that even small changes in precipitation and
+						temperature may result in profound landscape effects, particularly
+						if threshold values are crossed. For example, a shift from
+						below-freezing to above-freezing temperature can alter permafrost
+						and active layer, while hotter sping and summer temperatures can
+						increase fire frequency. Temperature and precipitation data are used
+						as inputs in many other models.
 					</p>
 					<p>
-						Many of these datasets were produced as part of the Integrated Ecosystem Management (IEM) Project.  <a href="https://uaf-snap.org/project/iem-an-integrated-ecosystem-model-for-alaska-and-northwest-canada/">Read more about this project</a>, or <a href="http://ckan.snap.uaf.edu/dataset?tags=IEM">access these datasets</a>.
+						Many of these datasets were produced as part of the Integrated
+						Ecosystem Management (IEM) Project.
+						<a
+							href="https://uaf-snap.org/project/iem-an-integrated-ecosystem-model-for-alaska-and-northwest-canada/"
+							>Read more about this project</a
+						>, or
+						<a href="http://ckan.snap.uaf.edu/dataset?tags=IEM"
+							>access these datasets</a
+						>.
 					</p>
 				</div>
+			</div>
+			<hr />
+			<div class="back">
+				<b-button
+					class="default"
+					tag="nuxt-link"
+					to="/#controls"
+					type="is-info"
+					icon-left="arrow-left-circle"
+				>
+					<strong>Go back</strong>, pick another place</b-button
+				>
 			</div>
 		</section>
 	</div>
@@ -141,6 +183,7 @@ export default {
 		}
 		this.results = await this.$http.$get(queryUrl)
 		this.originalData = this.results // save a copy!
+		this.units = 'imperial'
 	},
 	watch: {
 		units: function () {
@@ -152,10 +195,10 @@ export default {
 					(value, key, context) => {
 						if (key == 'pr') {
 							// Convert to inches!
-							return (value * 0.03937008).toFixed(2)
+							return parseFloat((value * 0.03937008).toFixed(2))
 						} else if (key == 'tas') {
 							// Convert to degrees F!
-							return (value * 1.8 + 32).toFixed(1)
+							return parseFloat((value * 1.8 + 32).toFixed(1))
 						}
 					},
 					{
