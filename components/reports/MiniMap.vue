2bd5c0eb4003743e626736873570847de2d91a71
Bob Torgerson <rltorgerson@alaska.edu>
2021-10-28 11:04:23 -0800

    Modified the MiniMap to use the USGS base layer, identical to the Arctic EDS MiniMap.

--- components/reports/MiniMap.vue
+++ components/reports/MiniMap.vue
@@ -54,39 +54,28 @@ export default {
 			this.map.fitBounds(this.geoJsonLayer.getBounds())
 		},
 		getBaseMapAndLayers() {
-			var baseLayer = new this.$L.tileLayer.wms(process.env.geoserverUrl, {
-				transparent: true,
-				srs: 'EPSG:3338',
-				format: 'image/png',
-				version: '1.3.0',
-				layers: ['atlas_mapproxy:alaska_osm'],
-			})
-
-			// Projection definition.
-			var proj = new this.$L.Proj.CRS(
-				'EPSG:3338',
-				'+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
+			var baseLayer = new L.tileLayer.wms(
+				"https://basemap.nationalmap.gov/arcgis/services/USGSTopo/MapServer/WmsServer?",
 				{
-					resolutions: [4096, 2048, 1024, 512, 256, 128, 64],
+					transparent: true,
+					format: "image/png",
+					version: "1.3.0",
+					layers: ["0"]
 				}
-			)
-
+			);
 			// Map base configuration
 			var config = {
-				zoom: 3,
+				zoom: 11,
 				minZoom: 0,
 				maxZoom: 6,
 				center: [64.7, -155],
 				scrollWheelZoom: false,
-				crs: proj,
-				continuousWorld: true,
 				zoomControl: false,
 				doubleClickZoom: false,
 				attributionControl: false,
-				layers: [baseLayer],
-			}
-
-			return config
+				layers: [baseLayer]
+			};
+			return config;
 		},
 	},
 }
