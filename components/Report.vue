8d32d3970d2ad56c86b2c79adf7769aca3d45462
BobTorgerson <rltorgerson@alaska.edu>
2021-10-28 13:13:46 -0800

    Qualitative text (#37)
    Adds a dynamically-generated descriptive text block that highlights key aspects of the data.

--- components/Report.vue
+++ components/Report.vue
@@ -1,76 +1,87 @@
 <template>
 	<div id="results" class="container">
-		<section class="section">
-			<div class="back">
-				<b-button
-					class="default"
-					tag="nuxt-link"
-					to="/#controls"
-					type="is-info"
-					icon-left="arrow-left-circle"
-				>
-					<strong>Go back</strong>, pick another place</b-button
-				>
-			</div>
-			<hr />
-			<h3 class="title is-4 place">
-				Projected Conditions for <span v-html="place"></span>
-			</h3>
-
-			<div v-if="$fetchState.pending">
-				<!-- Drama dots -->
-				<h4 class="title is-5">Loading data&hellip;</h4>
-				<b-progress type="is-info"></b-progress>
-			</div>
-			<div v-else-if="$fetchState.error" class="error">
-				<p class="content is-size-5">
-					Oh no! Something&rsquo;s amiss and the report for this place
-					couldn&rsquo;t be loaded.
-				</p>
-				<b-button
-					class="is-warning"
-					tag="nuxt-link"
-					to="/#controls"
-					icon-left="emoticon-sad-outline"
-				>
-					<strong>We&rsquo;re sorry</strong>, please try again</b-button
-				>
-			</div>
-
-			<div v-else>
-				<!-- The report! -->
-				<div class="report--minimap--wrapper">
-					<MiniMap />
-				</div>
-				<div class="report-controls">
-					<div class="content is-size-5">
-						<p>
-							The tables and charts below are specific to the selected point, or
-							have been averaged across the selected HUC or ecoregion. Note that
-							the precision of these results depends on the grid resolution
-							(pixel size) of the underlying dataset. Also note that some data
-							layers are not available as point data or at the HUC or ecoregion
-							level.
-						</p>
-						<p>
-							These data have been averaged by season.
-							<strong>Winter</strong> is December, January and February.
-							<strong>Spring</strong> is March, April and May.
-							<strong>Summer</strong> is June, July and August, and
-							<strong>Fall</strong> is September, October and November.
-						</p>
+		<div class="back">
+			<b-button
+				class="default"
+				tag="nuxt-link"
+				to="/#controls"
+				type="is-info"
+				icon-left="arrow-left-circle"
+			>
+				<strong>Go back</strong>, pick another place</b-button
+			>
+		</div>
+		<hr />
+		<div v-if="$fetchState.pending">
+			<!-- Drama dots -->
+			<h4 class="title is-5">Loading data&hellip;</h4>
+			<b-progress type="is-info"></b-progress>
+		</div>
+		<div v-else-if="$fetchState.error" class="error">
+			<p class="content is-size-5">
+				Oh no! Something&rsquo;s amiss and the report for this place
+				couldn&rsquo;t be loaded.
+			</p>
+			<b-button
+				class="is-warning"
+				tag="nuxt-link"
+				to="/#controls"
+				icon-left="emoticon-sad-outline"
+			>
+				<strong>We&rsquo;re sorry</strong>, please try again</b-button
+			>
+		</div>
+		<div v-else>
+			<!-- The report! -->
+			<section class="section">
+				<h3 class="title is-4 place centered">
+					Projected Future Conditions for <span v-html="place"></span>
+				</h3>
+				<QualitativeText :reportData="results" :place="place" :units="units" />
+			</section>
+			<section class="section">
+				<div class="columns">
+					<div class="column is-one-quarter">
+						<MiniMap />
 					</div>
-					<b-field label="Units">
-						<b-radio v-model="units" name="units" native-value="imperial">
-							Imperial
-						</b-radio>
-						<b-radio v-model="units" name="units" native-value="metric">
-							Metric
-						</b-radio>
-					</b-field>
+					<div class="column is-three-quarters">
+						<div class="report-controls">
+							<div class="content is-size-5">
+								<p>
+									The tables and charts below are specific to the selected
+									point, or have been averaged across the selected HUC or
+									ecoregion. Note that the precision of these results depends on
+									the grid resolution (pixel size) of the underlying dataset.
+									Also note that some data layers are not available as point
+									data or at the HUC or ecoregion level.
+								</p>
+								<p>
+									These data have been averaged by season.
+									<strong>Winter</strong> is December, January and February.
+									<strong>Spring</strong> is March, April and May.
+									<strong>Summer</strong> is June, July and August, and
+									<strong>fall</strong> is September, October and November.
+								</p>
+							</div>
+							<b-field label="Units">
+								<b-radio v-model="units" name="units" native-value="imperial">
+									Imperial
+								</b-radio>
+								<b-radio v-model="units" name="units" native-value="metric">
+									Metric
+								</b-radio>
+							</b-field>
+						</div>
+					</div>
 				</div>
+			</section>
+			<section class="section">
 				<div class="report-type-wrapper">
-					<TempReport :reportData="results" :units="units"></TempReport>
+					<TempReport
+						:reportData="results"
+						:place="place"
+						:units="units"
+					></TempReport>
 				</div>
 				<div class="report-type-wrapper">
 					<PrecipReport :reportData="results" :units="units"></PrecipReport>
@@ -100,7 +111,7 @@
 						>.
 					</p>
 				</div>
-			</div>
+			</section>
 			<hr />
 			<div class="back">
 				<b-button
@@ -113,27 +124,21 @@
 					<strong>Go back</strong>, pick another place</b-button
 				>
 			</div>
-		</section>
+		</div>
 	</div>
 </template>
 <style lang="scss" scoped>
+.centered {
+	text-align: center;
+}
 .report-controls {
 	margin-bottom: 1.25rem;
 }
 .report-type-wrapper {
 	margin: 1rem 0 2.5rem;
 }
-::v-deep table.table {
-	margin-top: -1rem;
-}
 .back {
 	margin-bottom: 2rem;
-}
-.place {
-	::v-deep .watershed {
-		color: #888;
-		font-weight: 300;
-	}
 }
 .b-radio.radio:not(.button) {
 	margin-right: 1rem;
@@ -144,6 +149,7 @@
 import TempReport from '~/components/reports/temperature/TempReport'
 import PrecipReport from '~/components/reports/precipitation/PrecipReport'
 import MiniMap from '~/components/reports/MiniMap'
+import QualitativeText from '~/components/reports/QualitativeText'
 import { mapGetters } from 'vuex'
 import lodash from 'lodash'
 import deepdash from 'deepdash'
@@ -152,7 +158,7 @@ const _ = deepdash(lodash)
 
 export default {
 	name: 'Report',
-	components: { TempReport, PrecipReport, MiniMap },
+	components: { TempReport, PrecipReport, MiniMap, QualitativeText },
 	data() {
 		return {
 			originalData: undefined, // for the raw stuff back from API
