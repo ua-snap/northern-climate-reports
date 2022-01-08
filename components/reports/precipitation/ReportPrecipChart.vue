08387a360e94892e68d7cca899965e752f8324de
Craig Stephenson <crstephenson@alaska.edu>
2022-01-07 17:10:32 -0900

    Remove hover text from boxplots and add chart footers with explanations/credit.

--- components/reports/precipitation/ReportPrecipChart.vue
+++ components/reports/precipitation/ReportPrecipChart.vue
@@ -62,6 +62,7 @@ export default {
 					color: '#888888'
 				},
 				fillcolor: '#cccccc',
+				hoverinfo: 'skip',
 			}
 
 			let models = ['5modelAvg', 'MRI-CGCM3', 'CCSM4']
@@ -172,10 +173,36 @@ export default {
 					},
 				],
 				hovermode: 'x unified',
+				annotations: [],
 				legend: {
 					x: 1.03
 				},
+				margin: {
+					b: 120
+				},
+				height: 500,
 			}
+
+			let footer_y = -0.4
+			if (window.innerWidth < 1250) {
+				layout['xaxis'] = {
+					tickangle: 45,
+				}
+				layout['margin']['b'] = 160
+				footer_y = -0.65
+			}
+
+			layout.annotations.push({
+				x: 0.5,
+				y: footer_y,
+				xref: 'paper',
+				yref: 'paper',
+				showarrow: false,
+				text: 'The boxplot represents the interquartile range (IQR) of historical means for the season, from 1950-2009.<br /> \
+					The shaded gray region shows the extent of common variation for the historical period.<br /> \
+					Credit: Scenarios Network for Alaska + Arctic Planning, University of Alaska Fairbanks.<br /> \
+					Visit northernclimatereports.org for more information.',
+			})
 
 			this.$Plotly.newPlot('precip-chart', data_traces, layout, {
 				displaylogo: false,
