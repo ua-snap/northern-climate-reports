9ace5a317c4ad8f9fb7a18d0f9d1260708c335c6
Craig Stephenson <crstephenson@alaska.edu>
2022-02-02 11:37:34 -0900

    Remove unnecessary permafrost chart padding.

--- components/reports/permafrost/ReportAltFreezeChart.vue
+++ components/reports/permafrost/ReportAltFreezeChart.vue
@@ -3,11 +3,6 @@
 		<div id="permafrost-alt-freeze-chart" />
 	</div>
 </template>
-<style lang="scss" scoped>
-.permafrost-chart-wrapper {
-	padding-bottom: 0;
-}
-</style>
 <script>
 import _ from 'lodash'
 import { mapGetters } from 'vuex'
