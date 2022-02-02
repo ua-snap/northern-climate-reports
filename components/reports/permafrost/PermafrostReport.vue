9f05b65e8d7bc4e3c6888821362627e265fff12a
Craig Stephenson <crstephenson@alaska.edu>
2022-02-02 11:28:47 -0900

    Fix indentation of permafrost chart files.

--- components/reports/permafrost/PermafrostReport.vue
+++ components/reports/permafrost/PermafrostReport.vue
@@ -1,58 +1,58 @@
 <template>
-  <div>
-    <h4 class="title is-3">Permafrost</h4>
-    <div class="content is-size-5">
-      <span
-        v-show="
-          permafrostPresent && permafrostDisappears && !permafrostUncertain
-        "
-      >
-        Projected permafrost active layer thickness and ground freeze depth
-        through the end of the century is shown below. The active layer is the
-        layer of soil above permafrost that thaws seasonally.
-      </span>
-      <span
-        v-show="
-          permafrostPresent && !permafrostDisappears && !permafrostUncertain
-        "
-      >
-        Projected permafrost active layer thickness through the end of the
-        century is shown below. The active layer is the layer of soil above
-        permafrost that thaws seasonally.
-      </span>
-      <span
-        v-show="
-          !permafrostPresent && permafrostDisappears && !permafrostUncertain
-        "
-      >
-        There is no permafrost within three meters of the ground surface at this
-        location. Projected ground freeze depth through the end of the century
-        is shown below.
-      </span>
-      <span v-show="permafrostUncertain">
-        The presence or absence of permafrost could not be determined for this
-        location because the historical mean annual ground temperature falls
-        within the threshold of uncertainty. A chart of the historical and
-        projected mean annual ground temperature is provided below.
-      </span>
-      Results were produced by the GIPL 2.0 permafrost model using five separate
-      climate models and two different greenhouse gas scenarios or
-      Representative Concentration Pathways (RCPs). RCP4.5 is an optimistic
-      future, and RCP8.5 is more pessimistic but also more likely.
-      <nuxt-link :to="{ name: 'about' }"
-        >Read more about models and RCPs.</nuxt-link
-      >
-    </div>
-    <div class="chart-wrapper permafrost" v-show="this.permafrostPresent">
-      <ReportAltThawChart />
-    </div>
-    <div class="chart-wrapper permafrost" v-show="this.permafrostDisappears">
-      <ReportAltFreezeChart />
-    </div>
-    <div class="chart-wrapper permafrost" v-show="this.permafrostUncertain">
-      <ReportMagtChart />
-    </div>
-  </div>
+	<div>
+		<h4 class="title is-3">Permafrost</h4>
+		<div class="content is-size-5">
+			<span
+				v-show="
+					permafrostPresent && permafrostDisappears && !permafrostUncertain
+				"
+			>
+				Projected permafrost active layer thickness and ground freeze depth
+				through the end of the century is shown below. The active layer is the
+				layer of soil above permafrost that thaws seasonally.
+			</span>
+			<span
+				v-show="
+					permafrostPresent && !permafrostDisappears && !permafrostUncertain
+				"
+			>
+				Projected permafrost active layer thickness through the end of the
+				century is shown below. The active layer is the layer of soil above
+				permafrost that thaws seasonally.
+			</span>
+			<span
+				v-show="
+					!permafrostPresent && permafrostDisappears && !permafrostUncertain
+				"
+			>
+				There is no permafrost within three meters of the ground surface at this
+				location. Projected ground freeze depth through the end of the century
+				is shown below.
+			</span>
+			<span v-show="permafrostUncertain">
+				The presence or absence of permafrost could not be determined for this
+				location because the historical mean annual ground temperature falls
+				within the threshold of uncertainty. A chart of the historical and
+				projected mean annual ground temperature is provided below.
+			</span>
+			Results were produced by the GIPL 2.0 permafrost model using five separate
+			climate models and two different greenhouse gas scenarios or
+			Representative Concentration Pathways (RCPs). RCP4.5 is an optimistic
+			future, and RCP8.5 is more pessimistic but also more likely.
+			<nuxt-link :to="{ name: 'about' }"
+				>Read more about models and RCPs.</nuxt-link
+			>
+		</div>
+		<div class="chart-wrapper permafrost" v-show="this.permafrostPresent">
+			<ReportAltThawChart />
+		</div>
+		<div class="chart-wrapper permafrost" v-show="this.permafrostDisappears">
+			<ReportAltFreezeChart />
+		</div>
+		<div class="chart-wrapper permafrost" v-show="this.permafrostUncertain">
+			<ReportMagtChart />
+		</div>
+	</div>
 </template>
 <style></style>
 <script>
@@ -62,14 +62,14 @@ import ReportMagtChart from './ReportMagtChart'
 import { mapGetters } from 'vuex'
 
 export default {
-  name: 'PermafrostReport',
-  components: { ReportAltThawChart, ReportAltFreezeChart, ReportMagtChart },
-  computed: {
-    ...mapGetters({
-      permafrostPresent: 'permafrost/present',
-      permafrostDisappears: 'permafrost/disappears',
-      permafrostUncertain: 'permafrost/uncertain',
-    }),
-  },
+	name: 'PermafrostReport',
+	components: { ReportAltThawChart, ReportAltFreezeChart, ReportMagtChart },
+	computed: {
+		...mapGetters({
+			permafrostPresent: 'permafrost/present',
+			permafrostDisappears: 'permafrost/disappears',
+			permafrostUncertain: 'permafrost/uncertain',
+		}),
+	},
 }
 </script>
