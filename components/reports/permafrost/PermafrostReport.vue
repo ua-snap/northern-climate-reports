a072bb7a2bbaeb3c8a30f9423495e78376e46847
Craig Stephenson <crstephenson@alaska.edu>
2022-02-18 13:48:11 -0900

    Rebase branch from main.

--- components/reports/permafrost/PermafrostReport.vue
+++ components/reports/permafrost/PermafrostReport.vue
@@ -14,9 +14,6 @@
             <span v-html="depthFragment"></span> of the ground surface over
             time.</strong
           >
-          Projected permafrost active layer thickness and ground freeze depth
-          through the end of the century is shown below. The active layer is the
-          layer of soil above permafrost that thaws seasonally.
         </span>
         <span
           v-show="
@@ -24,9 +21,6 @@
           "
           >Historical data and model projections indicate that
           <strong>this place has permafrost.</strong>
-          Projected permafrost active layer thickness through the end of the
-          century is shown below. The active layer is the layer of soil above
-          permafrost that thaws seasonally.
         </span>
         <span
           v-show="
@@ -37,32 +31,69 @@
             There is no permafrost within
             <span v-html="depthFragment"></span> of the ground surface at this
             location</strong
-          >. Projected ground freeze depth through the end of the century is
-          shown below.
-        </span>
+          >.</span
+        >
         <span v-show="permafrostUncertain">
           <strong
             >The presence or absence of permafrost could not be determined for
             this location</strong
-          >
-          because the historical mean annual ground temperature falls within the
-          threshold of uncertainty (<span v-html="uncertaintyFragment"></span>).
-          A chart of the historical and projected mean annual ground temperature
-          is provided below.
-        </span>
-        <nuxt-link :to="{ name: 'data', hash: '#datasets' }"
-          >See information about the dataset shown here.</nuxt-link
+          ></span
         >
+
+        <div class="mt-5">
+          The following maps show the historical and projected mean annual
+          ground temperature over time. This is the temperature of the soil
+          directly above the permafrost layer. Red represents temperatures above
+          freezing, blue represents temperatures below freezing, and white
+          represents temperatures near the freezing point. Darker reds represent
+          warmer temperatures. Darker blues represent colder temperatures.
+        </div>
       </div>
     </div>
-    <div class="chart" v-show="this.permafrostPresent">
-      <ReportAltThawChart />
-    </div>
-    <div class="chart" v-show="this.permafrostDisappears">
-      <ReportAltFreezeChart />
-    </div>
-    <div class="chart" v-show="this.permafrostUncertain">
-      <ReportMagtChart />
+    <ReportMagtMaps />
+
+    <div class="content">
+      <div class="is-size-5">
+        <span
+          v-show="
+            permafrostPresent && permafrostDisappears && !permafrostUncertain
+          "
+        >
+          Projected permafrost active layer thickness and ground freeze depth
+          through the end of the century is shown below. The active layer is the
+          layer of soil above permafrost that thaws seasonally.
+        </span>
+        <span
+          v-show="
+            permafrostPresent && !permafrostDisappears && !permafrostUncertain
+          "
+        >
+          Projected permafrost active layer thickness through the end of the
+          century is shown below. The active layer is the layer of soil above
+          permafrost that thaws seasonally.
+        </span>
+        <span
+          v-show="
+            !permafrostPresent && permafrostDisappears && !permafrostUncertain
+          "
+        >
+          Projected ground freeze depth through the end of the century is shown
+          below.
+        </span>
+        <span v-show="permafrostUncertain"
+          >A chart of the historical and projected mean annual ground
+          temperature is provided below.
+        </span>
+      </div>
+      <div class="chart" v-show="this.permafrostPresent">
+        <ReportAltThawChart />
+      </div>
+      <div class="chart" v-show="this.permafrostDisappears">
+        <ReportAltFreezeChart />
+      </div>
+      <div class="chart" v-show="this.permafrostUncertain">
+        <ReportMagtChart />
+      </div>
     </div>
   </div>
 </template>
@@ -73,6 +104,7 @@
 }
 </style>
 <script>
+import ReportMagtMaps from './ReportMagtMaps'
 import ReportAltThawChart from './ReportAltThawChart'
 import ReportAltFreezeChart from './ReportAltFreezeChart'
 import ReportMagtChart from './ReportMagtChart'
@@ -80,7 +112,12 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'PermafrostReport',
-  components: { ReportAltThawChart, ReportAltFreezeChart, ReportMagtChart },
+  components: {
+    ReportMagtMaps,
+    ReportAltThawChart,
+    ReportAltFreezeChart,
+    ReportMagtChart,
+  },
   computed: {
     // The range of uncertainty, within 1ºC of freezing.
     uncertaintyFragment() {
