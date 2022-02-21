6f2a6a81436db78c80bdd8628e5b59b71208011b
Craig Stephenson <crstephenson@alaska.edu>
2022-02-21 09:24:29 -0900

    Add input to switch between NCAR CCSM4 and MRI CGCM3 permafrost minimap models.

--- components/reports/permafrost/ReportMagtMaps.vue
+++ components/reports/permafrost/ReportMagtMaps.vue
@@ -1,5 +1,21 @@
 <template>
   <section class="section">
+    <div class="is-size-6 mb-4">
+      <b-field label="Model">
+        <b-radio
+          v-model="model_selection"
+          name="model_selection"
+          native-value="3"
+          >NCAR CCSM4</b-radio
+        >
+        <b-radio
+          v-model="model_selection"
+          name="model_selection"
+          native-value="4"
+          >MRI CGCM3</b-radio
+        >
+      </b-field>
+    </div>
     <div class="columns">
       <div class="column is-flex">
         <ReportMagtMap
@@ -10,25 +26,29 @@
         />
         <ReportMagtMap
           scenario="1"
-          model="1"
+          :model="model_selection"
+          :key="model_selection + '-rcp45-2025'"
           era="1"
           class="report--deltamap--map"
         />
         <ReportMagtMap
           scenario="1"
-          model="1"
+          :model="model_selection"
+          :key="model_selection + '-rcp45-2050'"
           era="2"
           class="report--deltamap--map"
         />
         <ReportMagtMap
           scenario="1"
-          model="1"
+          :model="model_selection"
+          :key="model_selection + '-rcp45-2075'"
           era="3"
           class="report--deltamap--map"
         />
         <ReportMagtMap
           scenario="1"
-          model="1"
+          :model="model_selection"
+          :key="model_selection + '-rcp45-2095'"
           era="4"
           class="report--deltamap--map"
         />
@@ -39,25 +59,29 @@
         <div class="report--deltamap--map" />
         <ReportMagtMap
           scenario="2"
-          model="1"
+          :model="model_selection"
+          :key="model_selection + '-rcp85-2025'"
           era="1"
           class="report--deltamap--map"
         />
         <ReportMagtMap
           scenario="2"
-          model="1"
+          :model="model_selection"
+          :key="model_selection + '-rcp85-2050'"
           era="2"
           class="report--deltamap--map"
         />
         <ReportMagtMap
           scenario="2"
-          model="1"
+          :model="model_selection"
+          :key="model_selection + '-rcp85-2075'"
           era="3"
           class="report--deltamap--map"
         />
         <ReportMagtMap
           scenario="2"
-          model="1"
+          :model="model_selection"
+          :key="model_selection + '-rcp85-2095'"
           era="4"
           class="report--deltamap--map"
         />
@@ -85,6 +109,11 @@ export default {
   name: 'ReportMagtMaps',
   components: {
     ReportMagtMap,
+  },
+  data() {
+    return {
+      model_selection: 3,
+    }
   },
 }
 </script>
