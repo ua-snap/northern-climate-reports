5a62d7648973bfdca5f1ad895163cd8fbe2c2f7a
Bruce Crevensten <bruce.crevensten@gmail.com>
2022-03-18 17:56:17 -0700

    JSPrettier for utils.js

--- utils/path.js
+++ utils/path.js
@@ -2,10 +2,17 @@ export const getAppPathFragment = function (type, id) {
   let path
   if (type == 'community') {
     path = '/report/community/' + id
-  } else if (type =='huc' || type == 'protected_area' || type == 'corporation' || type == 'climate_division' || type == 'ethnolinguistic_region' || type == 'fire_zone') {
+  } else if (
+    type == 'huc' ||
+    type == 'protected_area' ||
+    type == 'corporation' ||
+    type == 'climate_division' ||
+    type == 'ethnolinguistic_region' ||
+    type == 'fire_zone'
+  ) {
     path = '/report/area/' + id
   } else {
-    throw "Unknown path fragment type in utils/path.js"
+    throw 'Unknown path fragment type in utils/path.js'
   }
   return path
 }
