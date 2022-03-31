ac63e23d20a905090bec35b710c8045597f01b5d
Craig Stephenson <crstephenson@alaska.edu>
2022-03-30 19:57:48 -0800

    Add color key to vegetation change mini-maps.

--- components/reports/wildfire/ReportVegChangeMaps.vue
+++ components/reports/wildfire/ReportVegChangeMaps.vue
@@ -46,8 +46,94 @@
         <ReportVegChangeMap :model="veg_model_selection" scenario="3" era="3" />
       </div>
     </div>
+    <div class="legend">
+      <div class="color is-flex is-flex-direction-row">
+        <div class="swatch color-0" />
+        <div>Not Modeled</div>
+      </div>
+      <div class="color is-flex is-flex-direction-row">
+        <div class="swatch color-1" />
+        <div>Black Spruce</div>
+      </div>
+      <div class="color is-flex is-flex-direction-row">
+        <div class="swatch color-2" />
+        <div>White Spruce</div>
+      </div>
+      <div class="color is-flex is-flex-direction-row">
+        <div class="swatch color-3" />
+        <div>Deciduous Forest</div>
+      </div>
+      <div class="color is-flex is-flex-direction-row">
+        <div class="swatch color-4" />
+        <div>Shrub Tundra</div>
+      </div>
+      <div class="color is-flex is-flex-direction-row">
+        <div class="swatch color-5" />
+        <div>Graminoid Tundra</div>
+      </div>
+      <div class="color is-flex is-flex-direction-row">
+        <div class="swatch color-6" />
+        <div>Wetland Tundra</div>
+      </div>
+      <div class="color is-flex is-flex-direction-row">
+        <div class="swatch color-7" />
+        <div>Barren/Lichen/Moss</div>
+      </div>
+      <div class="color is-flex is-flex-direction-row">
+        <div class="swatch color-8" />
+        <div>Temperate Rainforest</div>
+      </div>
+    </div>
   </section>
 </template>
+
+<style lang="scss" scoped>
+.legend {
+  overflow: hidden;
+  max-width: 600px;
+  margin: 20px auto;
+  line-height: 1.2;
+}
+.color {
+  width: 200px;
+  float: left;
+  margin: 4px 0;
+}
+.swatch {
+  width: 20px;
+  height: 20px;
+  border: 1px solid #999999;
+  border-radius: 3px;
+  margin-right: 8px;
+}
+.color-0 {
+  background-color: #cccccc;
+}
+.color-1 {
+  background-color: #009459;
+}
+.color-2 {
+  background-color: #72cb88;
+}
+.color-3 {
+  background-color: #fdfeec;
+}
+.color-4 {
+  background-color: #ac916c;
+}
+.color-5 {
+  background-color: #ede0ce;
+}
+.color-6 {
+  background-color: #0d51fd;
+}
+.color-7 {
+  background-color: #f2a56e;
+}
+.color-8 {
+  background-color: #6de1e9;
+}
+</style>
 
 <script>
 import ReportVegChangeMap from './ReportVegChangeMap'
