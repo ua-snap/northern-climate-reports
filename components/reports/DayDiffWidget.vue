5deb75b7011a822750db91c0b35f5f693cf6d642
Bruce Crevensten <bruce.crevensten@gmail.com>
2023-06-20 16:57:37 -0700

    Modify coloring for precip indicators

--- components/reports/DayDiffWidget.vue
+++ components/reports/DayDiffWidget.vue
@@ -8,17 +8,12 @@
 <style lang="scss" scoped>
 .diff {
   display: block;
+  color: #05335e;
   &.weak {
     font-weight: 300;
   }
   &.strong {
     font-weight: 500;
-  }
-  &.more {
-    color: #5e3305;
-  }
-  &.less {
-    color: #05335e;
   }
 }
 </style>
@@ -41,10 +36,10 @@ export default {
     },
     // True if the difference is less than 10%, or 10mm
     isWeak() {
-      return Math.abs(this.pct) < 10
+      return Math.abs(this.pct) < 25
     },
     isStrong() {
-      return Math.abs(this.pct) > 20
+      return Math.abs(this.pct) > 50
     },
     isLess() {
       return this.pct < 0
