ac29cb8bf7586e80ca88799119486e23e2179332
Bruce Crevensten <bruce.crevensten@gmail.com>
2023-06-22 15:35:25 -0700

    Adds Place page + images

---
+++ pages/places.vue
@@ -0,0 +1,268 @@
+<template>
+  <section class="section">
+    <div class="content-wrapper">
+      <h1 class="title is-2 mb-6">
+        Places included in Northern Climate Reports
+      </h1>
+
+      <div class="card is-horizontal">
+        <div class="card-image is-small">
+          <figure class="image">
+            <img
+              src="~/assets/images/place_thumbnails/ancsa-corps_square.jpg"
+              alt="Example image showing Alaska Native Corporation boundaries"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <h3 class="title is-4">Alaska Native Corporations</h3>
+          <div class="content">
+            Like other states, Alaska has levels of government and land
+            ownership: federal, state, municipal, and city. The passage of the
+            Alaska Native Claims Settlement Act in 1971 added an additional
+            layer of land ownership across Alaska.
+            <a href="https://ancsaregional.com/the-twelve-regions/"
+              >Learn more about these regions.</a
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="card is-horizontal">
+        <div class="card-image is-small">
+          <figure class="image">
+            <img
+              src="~/assets/images/place_thumbnails/ak-census-borough_square.png"
+              alt="Example image of Alaska Boroughs and Census Areas"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <h3 class="title is-4">Boroughs and Census Areas</h3>
+          <div class="content">
+            There are 19 boroughs and 10 U.S. Census Bureau-designated census
+            areas in Alaska. These two systems have been aggregated into 29
+            geographic units which correspond to 2015 census area designations.
+            Alaska boroughs/census areas are equivalent to counties.
+            <a
+              href="https://en.wikipedia.org/wiki/List_of_boroughs_and_census_areas_in_Alaska"
+              >Read more about boroughs and census areas</a
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="card is-horizontal">
+        <div class="card-image is-small">
+          <figure class="image">
+            <img
+              src="~/assets/images/place_thumbnails/climate-divs_square.jpg"
+              alt="Example image of Alaska climate divisions"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <h3 class="title is-4">Climate Divisions</h3>
+          <div class="content">
+            Climate divisions are subdivisions of states having roughly
+            consistent climatological behavior within them.
+            <a
+              href="https://www.ncei.noaa.gov/access/monitoring/dyk/ak-climate-divisions"
+              >Learn more about climate divisions.</a
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="card is-horizontal">
+        <div class="card-image is-small">
+          <figure class="image">
+            <img
+              src="~/assets/images/place_thumbnails/ncr-communities_square.jpg"
+              alt="Map showing Alaska and Canada"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <h3 class="title is-4">Communities</h3>
+          <div class="content">
+            <p>
+              Search communities across Alaska and a portion of Canada by name.
+              The highlighted area of the map shows the extent of the
+              communities included in Northern Climate Reports.
+            </p>
+            <p>
+              We thank the
+              <a href="https://www.uaf.edu/anlc/index.php"
+                >Alaska Native Language Center</a
+              >
+              for providing a
+              <a href="https://www.uaf.edu/anla/collections/map/names/"
+                >reference of Alaska Native place names</a
+              >, and help with orthography.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card is-horizontal">
+        <div class="card-image is-small">
+          <figure class="image">
+            <img
+              src="~/assets/images/place_thumbnails/ethnoling_square.jpg"
+              alt="Example image of Alaska Ethnolinguistic regions"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <h3 class="title is-4">Ethnolinguistic Regions</h3>
+          <div class="content">
+            <p>
+              Language boundaries represent traditional territories at
+              approximately the year 1900 and are based on those established in
+              Michael E. Krauss' Native Peoples and Languages of Alaska map.
+              <a href="https://www.uaf.edu/anla/collections/map/"
+                >Learn more about Alaska Indigenous languages.</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card is-horizontal">
+        <div class="card-image is-small">
+          <figure class="image">
+            <img
+              src="~/assets/images/place_thumbnails/fire-mgmt_square.jpg"
+              alt="Example image of Alaska Fire Management Units"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <h3 class="title is-4">Fire Management Units</h3>
+          <div class="content">
+            <p>
+              The
+              <a href="https://akfireinfo.com/author/akforestry/"
+                >State of Alaska Department of Natural Resources</a
+              >, the US Bureau of Land Management Alaska Fire Service, and the
+              <a href="https://www.fs.usda.gov/managing-land/fire"
+                >US Forest Service</a
+              >
+              jointly manage the state to maximize efficient use of fire-related
+              resources.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card is-horizontal">
+        <div class="card-image is-small">
+          <figure class="image">
+            <img
+              src="~/assets/images/place_thumbnails/gmus-alaska_square.jpg"
+              alt="Example image of Alaska Game Management Units"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <h3 class="title is-4">Game Management Units</h3>
+          <div class="content">
+            <p>
+              The Alaska Department of Fish and Game manages hunting by dividing
+              the state into many Game Management Units (GMUs), each with
+              regulations on species, season, etc. Units are at the sub-unit
+              level.
+              <a
+                href="https://www.adfg.alaska.gov/index.cfm?adfg=huntingmaps.gmuinfo"
+                >Learn more about GMUs</a
+              >
+            </p>
+            <p class="photo-credit">Photo credit: UAF photo by Todd Paris</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card is-horizontal">
+        <div class="card-image is-small">
+          <figure class="image">
+            <img
+              src="~/assets/images/place_thumbnails/protected-areas_square.jpg"
+              alt="Example image of Alaska Protetect Area (Denali National Park)"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <h3 class="title is-4">Protected areas</h3>
+          <div class="content">
+            <p>
+              Protected areas include National Parks, National Forests,
+              Wilderness Areas, National Wildlife Refuges, State Parks and more,
+              searchable by name and agency (NPS, USFS, etc)
+            </p>
+            <p class="photo-credit">Photo credit: National Park Service</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card is-horizontal">
+        <div class="card-image is-small">
+          <figure class="image">
+            <img
+              src="~/assets/images/place_thumbnails/watershed-huc_square.jpg"
+              alt="An Alaskan landscape showing a watershed"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <h3 class="title is-4">Watersheds/Hydrological Units</h3>
+          <div class="content">
+            <p>
+              Hydrologic Units are included at the sub-basin (HU-8) and
+              watershed (HU-10) level, searchable by name or Hydrologic Unit
+              Code (HUC). Examples: &ldquo;Middle Copper River&rdquo; or
+              &ldquo;HUC 19020102.&rdquo; Some data (flammability, vegetation
+              change) are summarized at the sub-watershed (HU-12) level.
+              <a
+                href="https://en.wikipedia.org/wiki/Hydrologic_unit_system_(United_States)"
+                >Learn more about hydrological units</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card is-horizontal">
+        <div class="card-image is-small">
+          <figure class="image">
+            <img
+              src="~/assets/images/place_thumbnails/yukon-territories_square.jpg"
+              alt="Example map showing Yukon First Nation Traditional Territories"
+            />
+          </figure>
+        </div>
+        <div class="card-content">
+          <h3 class="title is-4">Yukon First Nation Traditional Territories</h3>
+          <div class="content">
+            <p>
+              There are 14 Yukon First Nations and 8 language groups. There are
+              also Northwest Territories and British Columbia Indigenous groups
+              that have traditional territory in Yukon. Approximately 25 percent
+              of Yukon’s population are Indigenous Peoples.
+              <a href="https://yukon.ca/en/find-out-about-yukon-first-nations"
+                >Learn more about Yukon First Nations</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+p.photo-credit {
+  font-size: 85%;
+  color: #888;
+}
+</style>
