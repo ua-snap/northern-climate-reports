86de987412b1d946a182f02f95310b681887db1a
Bruce Crevensten <bruce.crevensten@gmail.com>
2023-06-27 10:32:14 -0700

    Align beetle text with draft / other sections

--- components/reports/beetles/BeetleRiskReport.vue
+++ components/reports/beetles/BeetleRiskReport.vue
@@ -17,89 +17,19 @@
         <p>
           <a
             href="https://uaf-snap.org/wp-content/uploads/2023/06/20230508_spruce-beetle-climate-model-white-paper.pdf"
-            >Read a whitepaper about this model</a
-          >, or
-          <a
-            href="https://uaf-snap.org/project/modeling-spruce-bark-beetle-outbreaks-in-a-warming-climate/"
-            >additional information about the project</a
-          >.
+            >See more details about how the model was developed</a
+          >
         </p>
       </div>
     </div>
     <ReportBeetleRiskTable snowpack="low" />
     <ReportBeetleRiskTable snowpack="medium" />
     <ReportBeetleRiskMaps />
-
-    <div class="content">
-      <DownloadCsvButton
-        text="Download beetle risk data as CSV"
-        endpoint="beetles"
-        class="mt-3 mb-5"
-      />
-      <h4 class="title is-4">What&rsquo;s in the model</h4>
-      <div class="is-size-5">
-        <p>
-          This model incorporates three climate-based factors, which are
-          combined to calculate an overall outbreak risk for a given location
-          and year:
-        </p>
-        <ol>
-          <li>
-            Beetles&rsquo; rate of maturation in response to increasing summer
-            heat
-          </li>
-          <li>
-            Fall beetle survival in autumn based on rate of seasonal cooling
-          </li>
-          <li>
-            Survival of winter extreme cold, with or without insulating snow
-          </li>
-        </ol>
-        <p>
-          Modeled climate protection levels are based on conditions during more
-          than half the years in a given time period:
-        </p>
-        <ul>
-          <li>
-            <strong>high protection</strong> &equals; climate conditions are
-            likely to protect forests and prevent major outbreaks even if other
-            risk factors exist
-          </li>
-          <li>
-            <strong>minimal protection</strong> &equals; climate conditions may
-            provide some protection, but other risk factors can make outbreaks
-            likely
-          </li>
-          <li>
-            <strong>no protection</strong> &equals; climate is unlikely to limit
-            beetle populations
-          </li>
-        </ul>
-      </div>
-      <h4 class="title is-4 mt-6">What&rsquo;s not in the model</h4>
-      <div class="is-size-5">
-        <p>
-          This model does not include non-climate-related factors that may
-          affect outbreaks:
-        </p>
-        <ul>
-          <li>
-            Presence and percentage cover of the Alaska tree species (white
-            spruce, Sitka spruce Lutz spruce) most at risk of beetle infestation
-          </li>
-          <li>Forest age and successional stage</li>
-          <li>Forest health, including other stressors, pests, or blights</li>
-          <li>
-            Forest management, including fire management, harvest, and pest
-            control
-          </li>
-          <li>
-            Past and ongoing beetle outbreaks, including potential spread from
-            nearby regions
-          </li>
-        </ul>
-      </div>
-    </div>
+    <DownloadCsvButton
+      text="Download beetle risk data as CSV"
+      endpoint="beetles"
+      class="mt-3 mb-5"
+    />
   </div>
 </template>
 <script>
