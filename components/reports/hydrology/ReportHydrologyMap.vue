a252765dc1c00ccf72bee7d8c9195fc90f2f7f1a
Bob Torgerson <rltorgerson@alaska.edu>
2023-10-12 09:19:02 -0800

    Adds scenario as a watched variable for changing maps.

--- components/reports/hydrology/ReportHydrologyMap.vue
+++ components/reports/hydrology/ReportHydrologyMap.vue
@@ -96,6 +96,12 @@ export default {
       this.map.addLayer(this.baseLayer)
       this.baseLayer.bringToBack()
     },
+    scenario: function () {
+      this.map.removeLayer(this.baseLayer)
+      this.baseLayer = this.getBaseLayer()
+      this.map.addLayer(this.baseLayer)
+      this.baseLayer.bringToBack()
+    },
     // After geoJSON is loaded, display on map.
     geoJSON: function () {
       this.addGeoJSONtoMap()
