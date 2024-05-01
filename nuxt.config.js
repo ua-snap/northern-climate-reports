ebd0a06fdc7be0a1eb80f651336d19785acb4141
Craig Stephenson <crstephenson@alaska.edu>
2024-05-01 14:18:28 -0800

    Make SITE_OFFLINE environment variable case-insensitive.

--- nuxt.config.js
+++ nuxt.config.js
@@ -106,7 +106,7 @@ export default {
       process.env.RASDAMAN_URL || 'https://maps.earthmaps.io/rasdaman/ows',
     apiUrl: process.env.SNAP_API_URL || 'https://earthmaps.io',
     localStorageExpiration: 4,
-    offline: process.env.SITE_OFFLINE == 'true',
+    offline: process.env.SITE_OFFLINE?.toLowerCase() == 'true',
   },
 
   // Router customizations
