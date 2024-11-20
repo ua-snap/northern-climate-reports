cf5421ac73c2ef383163285afcf35b5dafaa1f36
Bruce Crevensten <bruce.crevensten@gmail.com>
2024-11-20 09:51:16 -0800

    Adapt code to read from API not WFS for main demographics

--- store/demographics.js
+++ store/demographics.js
@@ -38,10 +38,13 @@ export const mutations = {
 export const actions = {
   async fetch(context) {
     let placeId = context.rootGetters['place/communityId']
-    let queryUrl =
-      "https://gs.mapventure.org/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=demographics:demographics&outputFormat=application/json&cql_filter=id='" +
-      placeId +
-      "'"
+
+    // If this isn't a named community, don't try to fetch data.
+    if (!placeId) {
+      return
+    }
+    
+    let queryUrl = process.env.apiUrl + '/demographics/' + placeId
     let returnedData = await $axios
       .get(queryUrl, { timeout: 60000 })
       .catch(err => {
@@ -49,66 +52,25 @@ export const actions = {
         context.commit('setHttpError', 'no_data')
       })
 
-    // Test for a valid & non-empty response object
-    if (
-      _.isObject(returnedData.data) &&
-      _.isArray(returnedData.data.features) &&
-      _.isObject(returnedData.data.features[0]) &&
-      _.isObject(returnedData.data.features[0].properties) &&
-      _.isObject(returnedData.data.features[0].geometry)
-    ) {
-      // Valid response.
+    // We query for the geometry of the place separately
+    let geometryQueryUrl =
+      "https://gs.mapventure.org/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=demographics:demographics&outputFormat=application/json&cql_filter=id='" +
+      placeId +
+      "'"
+    let returnedGeometry = await $axios
+      .get(geometryQueryUrl, { timeout: 60000 })
+      .catch(err => {
+        console.error(err)
+        context.commit('setHttpError', 'no_data')
+      })
+
+    if (returnedData && returnedGeometry) {
       context.commit('setDemographicsData', {
-        // Sculpt the incoming field names to match nicer-to-read keys,
-        // field names from the shapefile are limited in length
-        // This won't necessarily be needed after we adapt the code
-        // to read from the API instead of a direct WFS query.
-        place: {
-          id: returnedData.data.features[0].properties.id,
-          name: returnedData.data.features[0].properties.name,
-          areatype: returnedData.data.features[0].properties.areatype,
-          placename: returnedData.data.features[0].properties.placename,
-          GEOID: returnedData.data.features[0].properties.GEOID,
-          total_population: returnedData.data.features[0].properties.total_popu,
-          pct_65_plus: returnedData.data.features[0].properties.pct_65_plu,
-          pct_under_18: returnedData.data.features[0].properties.pct_under_,
-          pct_under_5: returnedData.data.features[0].properties.pct_unde_1,
-          pct_hispanic_latino:
-            returnedData.data.features[0].properties.pct_hispan,
-          pct_white: returnedData.data.features[0].properties.pct_white,
-          pct_african_american:
-            returnedData.data.features[0].properties.pct_africa,
-          pct_amer_indian_ak_native:
-            returnedData.data.features[0].properties.pct_amer_i,
-          pct_asian: returnedData.data.features[0].properties.pct_asian,
-          pct_hawaiian_pacislander:
-            returnedData.data.features[0].properties.pct_hawaii,
-          pct_other: returnedData.data.features[0].properties.pct_other,
-          pct_multi: returnedData.data.features[0].properties.pct_multi,
-          pct_w_disability: returnedData.data.features[0].properties.pct_w_disa,
-          moe_pct_w_disability:
-            returnedData.data.features[0].properties.moe_pct_w_,
-          pct_insured: returnedData.data.features[0].properties.pct_insure,
-          moe_pct_insured: returnedData.data.features[0].properties.moe_pct_in,
-          pct_uninsured: returnedData.data.features[0].properties.pct_uninsu,
-          moe_pct_uninsured:
-            returnedData.data.features[0].properties.moe_pct_un,
-          pct_asthma: returnedData.data.features[0].properties.pct_asthma,
-          pct_copd: returnedData.data.features[0].properties.pct_copd,
-          pct_hd: returnedData.data.features[0].properties.pct_hd,
-          pct_stroke: returnedData.data.features[0].properties.pct_stroke,
-          pct_diabetes: returnedData.data.features[0].properties.pct_diabet,
-          pct_kd: returnedData.data.features[0].properties.pct_kd,
-          pct_minority: returnedData.data.features[0].properties.pct_minori,
-          pct_no_hsdiploma: returnedData.data.features[0].properties.pct_no_hsd,
-          pct_below_150pov: returnedData.data.features[0].properties.pct_below_,
-          pct_no_bband: returnedData.data.features[0].properties.pct_no_bba,
-          comment: returnedData.data.features[0].properties.comment,
-        },
-        alaska: demographics['AK0'],
-        us: demographics['US0'],
-        geometry: returnedData.data.features[0].geometry,
-      }) 
+        place: returnedData.data[placeId],
+        alaska: returnedData.data['AK0'],
+        us: returnedData.data['US0'],
+        geometry: returnedGeometry.data.features[0].geometry,
+      })
     } else {
       // No demographics for this place.
       context.commit('setHttpError', 'no_data')
