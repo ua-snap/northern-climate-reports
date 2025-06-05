e6c1d990c3839bfff38a28fa1d23067f5ac0a4f3
Craig Stephenson <crstephenson@alaska.edu>
2025-06-05 11:48:35 -0800

    Rename "Game Management Unit" and "Fire Management Unit" category labels, and combine Alaska/Yukon watersheds in map search results (#735)
    * Rename "Fire Management Unit" and "Game Management Unit" category labels to avoid ambiguity.
    
    * Combine HUCs and Yukon Watersheds in map search results.

--- pages/places.vue
+++ pages/places.vue
@@ -142,7 +142,7 @@
             </figure>
           </div>
           <div class="card-content">
-            <h3 class="title is-4">Fire Management Units</h3>
+            <h3 class="title is-4">Alaska Fire Management Units</h3>
             <div class="content">
               <p>
                 The
@@ -169,7 +169,7 @@
             </figure>
           </div>
           <div class="card-content">
-            <h3 class="title is-4">Game Management Units</h3>
+            <h3 class="title is-4">Alaska Game Management Units</h3>
             <div class="content">
               <p>
                 The Alaska Department of Fish and Game manages hunting by
@@ -251,8 +251,7 @@
                 Fire districts are administrative areas where wildland fire
                 resources are allocated and deployed. They fall within one of
                 six larger fire regions.
-                <a
-                  href="https://wildfires.service.yukon.ca/"
+                <a href="https://wildfires.service.yukon.ca/"
                   >Learn more about Yukon Fire Districts</a
                 >
               </p>
